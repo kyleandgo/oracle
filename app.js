4802122eb8607e786796653f2eb6d911bd3ab159
kyleandgo <38160353+kyleandgo@users.noreply.github.com>
2021-10-27 17:11:01 +0800

    Test test 1 2 3
    Test test 1 2 3

---
+++ app.js
@@ -0,0 +1,76 @@
+import { GlowParticle } from "./glowparticle.js";
+
+const COLORS = [
+    {r: 109, g: 114, b: 194}, // purple
+    {r: 254, g: 51, b: 0}, // orange
+    {r: 11, g: 71, b: 1}, // green
+];
+
+class App {
+    constructor(){
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+
+        this.pixelRatio = (window.devicePixelRatio > 1) ? 2: 1;
+
+        this.totalParticles = 200;
+        this.particles = [];
+        this.maxRadius = 120; 
+        this.minRadius = 100;
+
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+        window.requestAnimationFrame(this.animate.bind(this));
+    }
+
+    resize(){
+        this.stageWidth = document.body.clientWidth;
+        this.stageHeight = document.body.clientHeight;
+
+        this.canvas.width = this.stageWidth * this.pixelRatio;
+        this.canvas.height = this.stageHeight * this.pixelRatio;
+        this.ctx.scale(this.pixelRatio, this.pixelRatio);
+
+        this.createParticles();
+    }
+
+    createParticles(){
+        let curColor = 0;
+        this.particles = [];
+
+        for (let i = 0; i < this.totalParticles; i++){
+            const item = new GlowParticle(
+                Math.random() * this.stageWidth,
+                Math.random() * this.stageHeight,
+                Math.random() * (this.maxRadius - this.minRadius) + this.minRadius, 
+                COLORS[curColor]
+            );
+            
+            if (++curColor >= COLORS.length) {
+                curColor = 0;
+            }
+
+            this.particles[i] = item;
+        }
+
+    }
+
+    animate(){
+        window.requestAnimationFrame(this.animate.bind(this));
+
+        this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+        for (let i = 0; i < this.totalParticles; i++) {
+            const item = this.particles[i];
+            item.animate(this.ctx, this.stageWidth, this.stageHeight);
+
+        }
+
+    }
+}
+
+window.onload = () => {
+    new App();
+}
